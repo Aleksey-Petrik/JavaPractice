a1e9f2cf0d254283ad9c77b8e55832910e5dab9d
Aleksey Petrik <alexey.petric@gmail.com>
2021-08-03 17:10:54 +1000

    class MainPrintSortedParam added Comparator and lambdas.

--- Academy.Kovalevskyi.CodingBootcamp/src/week2/day0/MainPrintSortedParam.java
+++ Academy.Kovalevskyi.CodingBootcamp/src/week2/day0/MainPrintSortedParam.java
@@ -1,20 +1,22 @@
 package week2.day0;
 
+import week1.day1.Sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MainPrintSortedParam {
     public static void main(String[] args) {
         if (args.length != 0) {
-            for (int i = 1; i < args.length; i++) {
-                for (int j = 0; j < args.length - 1; j++) {
-                    if (args[j].compareTo(args[j + 1]) > 0) {
-                        String buf = args[j];
-                        args[j] = args[j + 1];
-                        args[j + 1] = buf;
-                    }
-                }
-            }
-            for (String arg : args) {
-                System.out.println(arg);
-            }
+            Comparator<String> comparator = String::compareTo;
+
+            Sorting.sort(args, comparator);//Ранее написанный метод в классе Sorting
+            Arrays.stream(args).forEach(System.out::println);//Сортировка по возрастанию
+
+            Sorting.sortReversedOrder(args, comparator);
+            Arrays.stream(args).forEach(System.out::println);//Сортировка по убыванию
+
+            Arrays.stream(args).sorted(String::compareTo).forEach(System.out::println);//В одной строке сортировка и вывод
         } else {
             System.out.println("Please specify at least one argument!");
         }

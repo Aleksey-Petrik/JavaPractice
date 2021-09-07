de685bdfe86146156b69b756a075c4c5b7e42e11
Aleksey Petrik <alexey.petric@gmail.com>
2021-09-07 22:42:41 +1000

    refactoring and new tests.

--- Academy.Kovalevskyi.JavaDeepDive/src/test/java/academy/kovalevskyi/javadeepdive/week0/day0/StdBufferedReaderTest.java
+++ Academy.Kovalevskyi.JavaDeepDive/src/test/java/academy/kovalevskyi/javadeepdive/week0/day0/StdBufferedReaderTest.java
@@ -1,25 +1,81 @@
 package academy.kovalevskyi.javadeepdive.week0.day0;
 
+import com.google.common.truth.Truth;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import java.io.CharArrayReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 class StdBufferedReaderTest {
 
   @Test
-  void readLine() throws IOException {
-    //Reader reader = new CharArrayReader("asdfasdfsafasdfsad asfasdfsadfsdafsda".toCharArray());
-    //Reader reader = new CharArrayReader(
-    //        "sadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadf\nasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;\nsdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sd".toCharArray());
-    Reader reader = new CharArrayReader("hello world\nhello hell hel\nhe".toCharArray());
-   //Reader reader = new CharArrayReader("\n\n\n\n".toCharArray());
+  void readLineNotNewLine() throws IOException {
+    char[] expectedChars = "asdfasdfsafasdfsad asfasdfsadfsdafsda".toCharArray();
+    char[] actualChars = new char[]{};
+    Reader reader = new CharArrayReader(expectedChars);
     StdBufferedReader sbr2 = new StdBufferedReader(reader, 4);
 
     while (sbr2.hasNext()) {
-      System.out.println(sbr2.readLine());
+      actualChars = sbr2.readLine();
     }
 
+    Truth.assertWithMessage("").that(actualChars).isEqualTo(expectedChars);
   }
+
+  @Test
+  void readLineLongLines() throws IOException {
+    Reader reader = new CharArrayReader(
+            "sadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadf\nasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;\nsdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sd".toCharArray());
+    List<char[]> expectedChars = new ArrayList<>();
+    StdBufferedReader sbr2 = new StdBufferedReader(reader, 4);
+
+    expectedChars.add("sadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadf".toCharArray());
+    expectedChars.add("asdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;".toCharArray());
+    expectedChars.add("sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sdsadfasdfsadfh;asfhda;sd".toCharArray());
+
+    int counter = 0;
+    while (sbr2.hasNext()) {
+      Truth.assertWithMessage("").that(sbr2.readLine()).isEqualTo(expectedChars.get(counter++));
+    }
+
+    Truth.assertWithMessage("").that(counter).isEqualTo(3);
+  }
+
+  @Test
+  void readLineSmallLines() throws IOException {
+    Reader reader = new CharArrayReader("hello world\nhello hell hel\nhe".toCharArray());
+    List<char[]> expectedChars = new ArrayList<>();
+    StdBufferedReader sbr2 = new StdBufferedReader(reader);
+
+    expectedChars.add("hello world".toCharArray());
+    expectedChars.add("hello hell hel".toCharArray());
+    expectedChars.add("he".toCharArray());
+
+    int counter = 0;
+    while (sbr2.hasNext()) {
+      Truth.assertWithMessage("").that(sbr2.readLine()).isEqualTo(expectedChars.get(counter++));
+    }
+
+    Truth.assertWithMessage("").that(counter).isEqualTo(3);
+  }
+
+  @Test
+  void readLineNullException() {
+    NullPointerException thrown = assertThrows(NullPointerException.class, () ->
+      new StdBufferedReader(null), "Error");
+    assertTrue(thrown.getMessage().contains("Reader is not be NULL!!!"));
+  }
+
+  @Test
+  void readLineSmallBufferSize() {
+    IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () ->
+            new StdBufferedReader(new CharArrayReader("bla bla".toCharArray()), -1 ), "Error");
+    assertTrue(thrown.getMessage().contains("Buffer size is not be less 2!!!"));
+  }
+
 }
